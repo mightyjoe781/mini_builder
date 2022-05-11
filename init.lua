d98b860784981f14423c5c6ec2234dd858a76e5b
smk <sudhanshumohan781@gmail.com>
2022-05-12 02:23:45 +0530

    Removed Translation Support

--- init.lua
+++ init.lua
@@ -1,11 +1,9 @@
 -- Load support for MT game translation.
-local S = minetest.get_translator("creative")
 local MP = minetest.get_modpath(minetest.get_current_modname())
 
 local mbuild = {
     items = {},
 }
-mbuild.get_translator = S
 
 -- define a function that can reload mbuild.items tables
 local function reload_mbuild()
@@ -38,69 +36,69 @@ end
 reload_mbuild()
 
 minetest.register_privilege("builder", {
-	description = S("Allow player to use restricted creative inventory"),
+	description = "Allow player to use restricted creative inventory",
 	give_to_singleplayer = false,
 	give_to_admin = false,
 })
 
 minetest.register_chatcommand("bgive",{
-    params = S("<item> <count>"),
-    description = S("Give player item(s)"),
+    params = "<item> <count>",
+    description = "Give player item(s)",
     privillage = "builder",
     func = function(name, param)
         local player = minetest.get_player_by_name(name)
         if not player then
-            return false, S("Player not found")
+            return false, "Player not found"
         end
         local params = param:split(" ")
         if #params < 2 then
-            return false, S("Invalid parameters")
+            return false, "Invalid parameters"
         end
         local item = params[1]
         local count = tonumber(params[2])
         if not item or not count then
-            return false, S("Invalid parameters")
+            return false, "Invalid parameters"
         end
         local inv = player:get_inventory()
         if not inv then
-            return false, S("Player inventory not found")
+            return false, "Player inventory not found"
         end
         -- check if items are in allowed inventory list
         if not mbuild.items[item] then
-            return false, S("Item not allowed")
+            return false, "Item not allowed"
         end
         -- before adding to inventory check if player has enough space
         if not inv:room_for_item("main", {name=item, count=count}) then
-            return false, S("Player inventory is full")
+            return false, "Player inventory is full"
         end
         -- check if item exists in minetest global registered nodes table
         if not minetest.registered_items[item] then
-            return false, S("Item not found")
+            return false, "Item not found"
         end
         inv:add_item("main", item.." "..count)
         -- log the action
         minetest.log("action", name.." gave "..count.." "..item.." to "..player:get_player_name())
-        return true, S("Item(s) given")
+        return true, "Item(s) given"
     end
 })
 
 -- chatcommand to reload mbuild.items table
 minetest.register_chatcommand("bgive_reload",{
-    params = S(""),
-    description = S("Reload mbuild items table"),
+    params = "",
+    description = "Reload mbuild items table",
     func = function(name, param)
         -- allow no parameters
         if param ~= "" then
-            return false, S("Invalid parameters")
+            return false, "Invalid parameters"
         end
         -- check auth level of person
         if not minetest.check_player_privs(name, {server=true}) then
-            return false, S("You are not allowed to reload items table")
+            return false, "You are not allowed to reload items table"
         end
         reload_mbuild()
-        return true, S("Items table reloaded")
+        return true, "Items table reloaded"
     end
 })
 
 -- print OK on startup
-print(S("[mini_builder] Loaded"))
+print("[mini_builder] Loaded")
